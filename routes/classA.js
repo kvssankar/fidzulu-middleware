0f1160b5f29f3ccae2a5e43f166e59f7ecf2dc68
kvssankar <kvs.sankar001@gmail.com>
2023-10-12 09:48:25 +0530

    done

--- routes/classA.js
+++ routes/classA.js
@@ -2,33 +2,6 @@ const axios = require("axios");
 const { ROUTE_MAP, ROUTE_MAP_TEAMS } = require("../constants/routemap");
 const router = require("express").Router();
 
-router.get("/:servicename/all/:location", async (req, res) => {
-  const servicename = req.params.servicename;
-  try {
-    const url = `${ROUTE_MAP[servicename]}/${req.params.location}`;
-    const response = await axios.get(url);
-    res.json(response.data);
-  } catch (err) {
-    const status = err?.response?.status || 500;
-    const data = err?.response?.data || { message: "Internal Server Error" };
-    res.status(status).json(data);
-  }
-});
-
-router.get("/:servicename/team", async (req, res) => {
-  const servicename = req.params.servicename;
-  try {
-    const url = `${ROUTE_MAP_TEAMS[servicename]}`;
-    console.log(url);
-    const response = await axios.get(url);
-    res.json(response.data);
-  } catch (err) {
-    const status = err?.response?.status || 500;
-    const data = err?.response?.data || { message: "Internal Server Error" };
-    res.status(status).json(data);
-  }
-});
-
 router.get("/all/team", async (req, res) => {
   res.json([
     {
@@ -83,6 +56,33 @@ router.get("/all/team", async (req, res) => {
       },
     },
   ]);
+});
+
+router.get("/:servicename/all/:location", async (req, res) => {
+  const servicename = req.params.servicename;
+  try {
+    const url = `${ROUTE_MAP[servicename]}/${req.params.location}`;
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    const status = err?.response?.status || 500;
+    const data = err?.response?.data || { message: "Internal Server Error" };
+    res.status(status).json(data);
+  }
+});
+
+router.get("/:servicename/team", async (req, res) => {
+  const servicename = req.params.servicename;
+  try {
+    const url = `${ROUTE_MAP_TEAMS[servicename]}`;
+    console.log(url);
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    const status = err?.response?.status || 500;
+    const data = err?.response?.data || { message: "Internal Server Error" };
+    res.status(status).json(data);
+  }
 });
 
 module.exports = router;
